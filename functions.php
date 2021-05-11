2726355a97286c2fbb51506bca851bfb5a8368e5
ryancortis-b42697 <ryan.cortis.b42697@mcast.edu.mt>
2021-05-11 19:11:08 +0200

    CMS Final GIT Commit
    Final Commit

--- functions.php
+++ functions.php
@@ -1,4 +1,12 @@
 <?php 
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' ); 
+function my_theme_enqueue_styles() {
+    $parent_style = 'customify'; 
+    $child_style = 'manual-child';
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( $child_style, get_stylesheet_uri() );
+}
+
 function twitter_button_shortcode() {
     return '<a href="http://twitter.com/mysupps" class="twitter-button">Follow us on Twitter!</a>';
 }
